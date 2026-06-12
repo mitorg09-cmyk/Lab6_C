693c04b5113b8487eed60763ace4cf462c001f10
Mitrofan <mityirg09@mail.ru>
2026-06-12 07:20:31 -0400

    Added bases of 3 functions: push, pop, destroy

--- main.c
+++ main.c
@@ -1,7 +1,47 @@
 #include <stdio.h>
 
+struct point
+{
+  double val;
+  struct point* next;
+};
+
+struct stack
+{
+  struct point* first;
+  size_t size;
+};
+
+int push(struct stack* stk, double val);
+int pop(struct stack* stk, double* val);
+int destroy(struct stack** stk);
+
 int main()
 {
-    printf("Hello World!\n");
-    return 0;
+
+
+  return 0;
+}
+
+int push(struct stack* stk, double val)
+{
+  if(!stk) return -1;
+
+
+}
+
+int pop(struct stack* stk, double* val)
+{
+  if(!stk) return -1;
+  if(!(stk->size)) return -1;
+
+
+}
+
+int destroy(struct stack** stk)
+{
+  if(!stk) return -1;
+  if(!*stk) return 1;
+
+
 }
