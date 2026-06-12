c4b0f5b6193fa77227c2ef3e22eae2aed0f01af8
Mitrofan <mityirg09@mail.ru>
2026-06-12 08:16:44 -0400

    Added logic for all functions

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct point
 {
@@ -14,7 +15,7 @@ struct stack
 
 int push(struct stack* stk, double val);
 int pop(struct stack* stk, double* val);
-int destroy(struct stack** stk);
+int destroyStack(struct stack** stk);
 int readHead(struct stack*, double* val);
 
 int main()
@@ -28,7 +29,16 @@ int push(struct stack* stk, double val)
 {
   if(!stk) return -1;
 
+  struct point* newPoint = (struct point*)malloc(sizeof(struct point));
+  if(!newPoint) return 2;
 
+  newPoint->val = val;
+  newPoint->next = stk->first; // if new stack than NULL
+  stk->first = newPoint;
+
+  stk->size++;
+
+  return 0;
 }
 
 int pop(struct stack* stk, double* val)
@@ -36,19 +46,40 @@ int pop(struct stack* stk, double* val)
   if(!stk) return -1;
   if(!(stk->size)) return -1;
 
+  *val = stk->first->val;
+  struct point* fHead = stk->first;
 
+  stk->first = stk->first->next; // if one elem than NULL
+  stk->size--;
 
+  free(fHead);
+
+  return 0;
 }
 
-int destroy(struct stack** stk)
+int destroyStack(struct stack* stk)
+{
+  if(!stk) return 1;
+
+  struct point* fHead = NULL;
+  while(stk->first)
+  {
+    fHead = stk->first;
+    stk->first = stk->first->next;
+    stk->size--;
+
+    free(fHead);
+  }
+
+  return 0;
+}
+
+int readHead(struct stack* stk, double* val)
 {
   if(!stk) return -1;
-  if(!*stk) return 1;
+  if(!(stk->first)) return -1;
 
+  *val = stk->first->val;
 
-}
-
-int readHead(struct stack*, double* val)
-{
-
+  return 0;
 }
